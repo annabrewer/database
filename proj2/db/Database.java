47865f304a6e938c24428a109ada8c6bd56367f6
Anna  Brewer <Anna@calvisitor-10-105-141-58.calvisitor.1918.berkeley.edu>
2017-03-01 00:39:08 -0800

    progress on database, parser and table classes

--- proj2/db/Database.java
+++ proj2/db/Database.java
@@ -1,11 +1,43 @@
 package db;
 
+import java.util.ArrayList;
+
 public class Database {
+    ArrayList<Table> tables =
+
     public Database() {
         // YOUR CODE HERE
     }
 
     public String transact(String query) {
-        return "YOUR CODE HERE";
+        Parser p = new Parser();
+        Object[] input = p.eval(query);
+        String cmd = input[0]
+
+        if (cmd.equals("create new")) {
+            String name = (String)input[1];
+            String[] column = (String[])input[2];
+            tables.add(new Table(name, column));
+
+        } else if (cmd.equals("create selected")) {
+            String name = (String)input[1];
+            String[] expressions = (String[])input[2]
+            String[] columns = (String[])input[3];
+            String[] tables = (String[])input[4];
+            tables.add( //add the result of calling the select method, which should be in Database
+        } else if (cmd.equals("load")) {
+
+
+        } else if (cmd.equals("store")) {
+
+        } else if (cmd.equals("drop")) {
+
+        } else if (cmd.equals("insert")) {
+
+        } else if (cmd.equals("print")) {
+
+        } else if (cmd.equals("select")) {
+
+        }
     }
 }
